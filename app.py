8d0e864f12e8dc6b083959b1e071b634739feb92
kohki.sato <k48692003@yahoo.co.jp>
2021-07-09 17:06:24 +0900

    add some routing

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template 
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', username="John")
+
+@app.route('/routing')
+def route():
+    return 'hello routing page is working'
+
+
+@app.route('/user/<userid>')
+def show_user_profile(userid):
+    return 'the user id is {}'.format(userid)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'post id is {}'.format(post_id)
