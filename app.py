9481303d44899563e261fdabd9b8c95f57db861d
kohki.sato <k48692003@yahoo.co.jp>
2021-07-10 17:43:48 +0900

    add some routings

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template ,url_for, request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -19,3 +19,20 @@ def show_user_profile(userid):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'post id is {}'.format(post_id)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return 'post method is called'
+    else:
+        return 'get method is called'
+
+# debugging method to printout the url for each endpoint
+with app.test_request_context():
+    print('test_request_context method is called!')
+    print(url_for('hello_world'))
+    print(url_for('route'))
+
+    print(url_for('show_user_profile', userid='150'))
+    # print(url_for('profile', username='John Doe'))
