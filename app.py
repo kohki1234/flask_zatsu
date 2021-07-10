4c997410cffc4c9512f5bab621011aeb187c2fb4
kohki.sato <k48692003@yahoo.co.jp>
2021-07-10 23:26:02 +0900

    add database connection

--- app.py
+++ app.py
@@ -1,59 +1,42 @@
-from flask import Flask, render_template ,url_for, request, g
+from flask import Flask, render_template, g, request, redirect, url_for 
 from markupsafe import escape
 import os
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+db_uri = os.environ.get('DATABASE_URL') or "postgresql://localhost/flasknote"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app) 
+
+class Entry(db.Model): 
+    __tablename__ = "entries" 
+    id = db.Column(db.Integer, primary_key=True) 
+    title = db.Column(db.String(), nullable=False) 
+    body = db.Column(db.String(), nullable=False) 
+
 @app.route('/')
 def hello_world():
-    entries = get_db().execute('select title, body from entries').fetchall()
+    entries = Entry.query.all()
     return render_template('index.html', entries=entries)
 
 
-# connect database 
-def connect_db():
-    db_path = os.path.join(app.root_path,'flasknote.db')
-    rv = sqlite3.connect(db_path)
-    rv.row_factory = sqlite3.Row
-    return rv
+@app.route('/add')
+def add_comment():
+    entries = Entry.query.all()
+    return render_template('add.html', entries=entries)
 
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+@app.route('/post', methods=['POST'])
+def add_entry():
+    # create a new entry instance
+    entry = Entry()
 
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    # assign form information to columns
+    entry.title = request.form['title']
+    entry.body = request.form['body']
 
-@app.route('/routing')
-def route():
-    return 'hello routing page is working'
-
-
-@app.route('/user/<userid>')
-def show_user_profile(userid):
-    return 'the user id is {}'.format(userid)
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'post id is {}'.format(post_id)
-
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if request.method == 'POST':
-        return 'post method is called'
-    else:
-        return 'get method is called'
-
-# debugging method to printout the url for each endpoint
-with app.test_request_context():
-    print('test_request_context method is called!')
-    print(url_for('hello_world'))
-    print(url_for('route'))
-
-    print(url_for('show_user_profile', userid='150'))
-    # print(url_for('profile', username='John Doe'))
+    # add information
+    db.session.add(entry)
+    db.session.commit()
+    return redirect(url_for('hello_world'))
