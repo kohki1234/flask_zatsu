af70cb422ec0a3d4337ea3d6b5bfe978cee11bce
kohki.sato <k48692003@yahoo.co.jp>
2021-07-10 18:31:43 +0900

    add loop

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template ,url_for, request
+from flask import Flask, render_template ,url_for, request, g
 from markupsafe import escape
+import os
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', username="John")
+    entries = get_db().execute('select title, body from entries').fetchall()
+    return render_template('index.html', entries=entries)
+
+
+# connect database 
+def connect_db():
+    db_path = os.path.join(app.root_path,'flasknote.db')
+    rv = sqlite3.connect(db_path)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/routing')
 def route():
